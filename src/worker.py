89d775a11c9fdcb6bcd06d364bacb2f7e0db9369
r5 <r5@>
2025-06-13 23:26:58 +0800

    1

--- src/worker.py
+++ src/worker.py
@@ -38,35 +38,35 @@ async def get_count():
     global C
     return {"count": C}
 
-# @app.get("/env")
-# async def env(req: Request):
-#     env = req.scope["env"]
-#     return {
-#         "message": "Here is an example of getting an environment variable: "
-#         + env.MESSAGE
-#     }
+@app.get("/env")
+async def env(req: Request):
+    env = req.scope["env"]
+    return {
+        "message": "Here is an example of getting an environment variable: "
+        + env.MESSAGE
+    }
 
 
-# class Item(BaseModel):
-#     name: str
-#     description: str | None = None
-#     price: float
-#     tax: float | None = None
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
-# @app.post("/items/")
-# async def create_item(item: Item):
-#     return item
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 
-# @app.put("/items/{item_id}")
-# async def update_item(item_id: int, item: Item, q: str | None = None):
-#     result = {"item_id": item_id, **item.model_dump()}
-#     if q:
-#         result.update({"q": q})
-#     return result
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int):
-#     return {"item_id": item_id}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
