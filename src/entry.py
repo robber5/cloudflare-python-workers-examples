99836f7ac1cbbdf5cab3e959e4608836b34094e4
r5 <r5@>
2025-06-14 20:17:31 +0800

    1

--- src/entry.py
+++ src/entry.py
@@ -1,9 +1,5 @@
 from workers import Response
 
 
-from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 async def on_fetch(request, env):
-    import os
-    p = os.path.dirname(os.path.abspath(__file__))
-    a = [x for x in os.listdir(p)]
-    return Response(str(a))
+    return Response("Hello")
