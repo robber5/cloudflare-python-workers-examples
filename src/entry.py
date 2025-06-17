497f9bab0a96cb3b37a914e8a4264d99064a1f64
r5 <r5@>
2025-06-17 23:10:35 +0800

    1

--- src/entry.py
+++ src/entry.py
@@ -6,7 +6,13 @@ async def on_fetch(request, env):
     host = (await request.json()).host
     try:
         ip_address = socket.gethostbyname(host)
+        dns_info = socket.gethostbyaddr(ip_address)
     except socket.gaierror:
-        pass
+        ip_address = "Unable to resolve host"
+        dns_info = "No DNS information available"
+    except socket.herror:
+        dns_info = "No DNS information available"
 
-    return Response(f"Host: {host}, IP: {ip_address}")
+    return Response(f"Host: {host}, IP: {ip_address}, DNS Info: {dns_info}")
+
+
