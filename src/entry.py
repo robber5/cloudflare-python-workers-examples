ed5327672464be79b178124053e639b55a3a9138
r5 <r5@>
2025-06-17 22:58:34 +0800

    1

--- src/entry.py
+++ src/entry.py
@@ -1,5 +1,12 @@
 from workers import Response
-from fastapi import FastAPI
+import socket
 
 async def on_fetch(request, env):
-    return Response("Hello")
+    ip_address = None
+    host = (await request.json()).host
+    try:
+        ip_address = socket.gethostbyname(host)
+    except socket.gaierror:
+        pass
+
+    return Response(f"Host: {host}, IP: {ip_address}")
